23d629004862f991e2aa1e8aa566ede52d19cc85
Mathew877 <mbrevans@gmail.com>
2025-03-12 22:58:14 -0400

    limit draw to 60fps

--- main.py
+++ main.py
@@ -5,17 +5,20 @@ import pygame
 from constants import *
 
 def main():
-	print("Starting Asteroids!")
-	print(f"Screen width: {SCREEN_WIDTH}")
-	print(f"Screen height: {SCREEN_HEIGHT}")
-	numpass, numfail = pygame.init()
+	pygame.init()
 	screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+	clock = pygame.time.Clock()
+	dt = 0
+
 	while True:
 		for event in pygame.event.get():
 			if event.type == pygame.QUIT:
 				return
 			
 		screen.fill("black")
+		pygame.display.flip()
+
+		dt = clock.tick(60) / 1000
 
 
 
