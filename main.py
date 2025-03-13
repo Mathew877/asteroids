647658929ea0756a8a92ba1dbdbbcbf268d77156
Mathew877 <mbrevans@gmail.com>
2025-03-12 23:15:10 -0400

    Creates player and draws to screen

--- main.py
+++ main.py
@@ -3,12 +3,15 @@
 # throughout this file
 import pygame
 from constants import *
+from player import *
 
 def main():
 	pygame.init()
 	screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 	clock = pygame.time.Clock()
 	dt = 0
+	# spawn player in center of screen
+	player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2) 
 
 	while True:
 		for event in pygame.event.get():
@@ -16,6 +19,7 @@ def main():
 				return
 			
 		screen.fill("black")
+		player.draw(screen)
 		pygame.display.flip()
 
 		dt = clock.tick(60) / 1000
